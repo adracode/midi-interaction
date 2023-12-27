a056e029fa8cbc82aa1fde28eef98c4f22f42177
Adrackiin <adrackiin@gmail.com>
2023-12-27 15:51:48 +0100

    fix: when releasing keys too fast, they are not handled

--- src/main/java/fr/adracode/piano/keyboard/KeyboardSimulator.java
+++ src/main/java/fr/adracode/piano/keyboard/KeyboardSimulator.java
@@ -7,6 +7,8 @@ import fr.adracode.piano.keyboard.config.KeyboardSettings;
 import fr.adracode.piano.keyboard.config.MappingParser;
 import fr.adracode.piano.keyboard.config.RawMappingConfig;
 import fr.adracode.piano.keyboard.os.OSKeyboard;
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,12 +17,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static fr.adracode.piano.keyboard.KeyboardHand.OCTAVE_WITH_PEDALS;
 import static fr.adracode.piano.keyboard.KeyboardHand.TONE;
 
 public class KeyboardSimulator {
     private final KeyboardSettings settings;
+    private final Int2IntMap[] cache = new Int2IntMap[OCTAVE_WITH_PEDALS];
 
     private final ScheduledExecutorService timer = Executors.newScheduledThreadPool(1);
     private final KeyboardMapping keyboardMapping;
@@ -42,16 +46,9 @@ public class KeyboardSimulator {
         keyboardMapping = new KeyboardMapping(keyboard, parser.getKeys(), new KeyboardHand(settings.sustainAfter()),
                 Arrays.stream(toggleMasks).map(mask -> ~mask).toArray(), toggleManager);
 
-        System.out.println("Ready!");
-    }
+        IntStream.range(0, OCTAVE_WITH_PEDALS).forEach(i -> cache[i] = new Int2IntOpenHashMap());
 
-    public void handleKeyReleased(int data){
-        int octave = data / TONE;
-        if(isToggle(octave, data)){
-            toggleManager.getHand().unregisterKey(octave, data % TONE);
-        } else {
-            keyboardMapping.getHand().unregisterKey(octave, data % TONE);
-        }
+        System.out.println("Ready!");
     }
 
     private boolean isToggle(int octave, int tone){
@@ -59,6 +56,37 @@ public class KeyboardSimulator {
     }
 
     public void handleKeyPressed(int data1, int data2){
+        int octave = register(data1, data2);
+        cache[octave].put(data1, -data2);
+        if(!isSustain(octave)){
+            timerTask[octave] = timer.scheduleAtFixedRate(() -> {
+                try {
+                    synchronized(this){
+                        cache[octave].int2IntEntrySet().stream()
+                                .filter(entry -> entry.getIntValue() > 0)
+                                .forEach(entry -> register(entry.getIntKey(), entry.getIntValue()));
+
+                        toggleManager.handle(octave);
+                        keyboardMapping.handle(octave);
+
+                        if(toggleManager.getHand().isEmpty(octave) && keyboardMapping.getHand().isEmpty(octave)){
+                            timerTask[octave].cancel(true);
+                            timerTask[octave] = null;
+                        }
+
+                        cache[octave].int2IntEntrySet().stream()
+                                .filter(entry -> entry.getIntValue() > 0)
+                                .forEach(entry -> handleKeyReleased(entry.getIntKey()));
+                        cache[octave].clear();
+                    }
+                } catch(RuntimeException e){
+                    e.printStackTrace();
+                }
+            }, settings.keyInterval(), settings.sustainRepeat(), TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private int register(int data1, int data2){
         int octave = data1 / TONE;
         if(isToggle(octave, data1)){
             if(data2 < settings.toggleOnceBelow()){
@@ -68,26 +96,22 @@ public class KeyboardSimulator {
         } else {
             keyboardMapping.getHand().registerKey(octave, data1 % TONE);
         }
-        if(!isSustain(octave)){
-            //TODO: Si dans single, NE PEUT PAS être un premier trigger
-            //TODO: boutons toggle exclusifs à ça
-            timerTask[octave] = timer.scheduleAtFixedRate(() -> {
-                try {
-                    synchronized(this){
-                        toggleManager.handle(octave);
-                        keyboardMapping.handle(octave);
-                        if(toggleManager.getHand().isEmpty(octave) && keyboardMapping.getHand().isEmpty(octave)){
-                            timerTask[octave].cancel(true);
-                            timerTask[octave] = null;
-                        }
-                    }
-                } catch(RuntimeException e){
-                    e.printStackTrace();
-                }
-            }, settings.keyInterval(), settings.sustainRepeat(), TimeUnit.MILLISECONDS);
-        }
+        return octave;
     }
 
+    public void handleKeyReleased(int data){
+        int octave = data / TONE;
+        int cache = this.cache[octave].getOrDefault(data, 0);
+        //Hasn't been consumed
+        if(cache != 0){
+            this.cache[octave].replace(data, -cache);
+        }
+        if(isToggle(octave, data)){
+            toggleManager.getHand().unregisterKey(octave, data % TONE);
+        } else {
+            keyboardMapping.getHand().unregisterKey(octave, data % TONE);
+        }
+    }
 
     private boolean isSustain(int octave){
         return timerTask[octave] != null && !timerTask[octave].isCancelled();
